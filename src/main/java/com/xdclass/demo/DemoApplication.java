214da761bf282c85da90f4d2d66193786218d424
610987187 <41191693+610987187@users.noreply.github.com>
2020-06-14 01:32:59 +0800

    Update DemoApplication.java

--- src/main/java/com/xdclass/demo/DemoApplication.java
+++ src/main/java/com/xdclass/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         //sss
         //sourt
+        //99
         //ii
         //ooo
         SpringApplication.run(DemoApplication.class, args);
