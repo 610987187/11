f6fbb182eea89f85ccccf8f0a7061f1666ba9877
yuhuijuan1 <yuhuijuan1@example.com>
2020-06-19 01:53:53 +0800

    01

--- src/main/java/com/aa.java
+++ src/main/java/com/aa.java
@@ -1,15 +1,14 @@
 package com.xdclass.demo;
 
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class aa {
     @RequestMapping("/index")
-    public String hello()
-        1212
-        //cc
+
+    public String hello(){
+        //11
        return "h111111111111";
     }
 }
